9229e0bd0b45a95fb7195685495fa75202ba78a8
x-student-x <148026385+x-student-x@users.noreply.github.com>
2024-04-02 23:09:31 -0500

    up
    up

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -17,6 +17,8 @@ from .populate import initiate
 
 from .models import CarMake, CarModel
 
+from .restapis import get_request, analyze_review_sentiments, post_review
+
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
