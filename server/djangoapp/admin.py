1fef8735712f5cf55a43944d7732eb08bf446884
x-student-x <148026385+x-student-x@users.noreply.github.com>
2024-04-02 22:18:05 -0500

    update
    update

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,5 +1,5 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
@@ -11,3 +11,7 @@
 # CarMakeAdmin class with CarModelInline
 
 # Register models here
+
+# Registering models with their respective admins
+admin.site.register(CarMake)
+admin.site.register(CarModel)
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,18 +1,37 @@
 # Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from . import views
+from .restapis import get_request, analyze_review_sentiments, post_review
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
+    # path for registration
+    path(route='registration', view=views.login_user, name='registration'),
 
     # path for login
-    # path(route='login', view=views.login_user, name='login'),
+    path(route='login', view=views.login_user, name='login'),
+
+    # path for logout
+    path(route='logout', view=views.login_user, name='logout'),
+
+    # path for get_cars
+    path(route='get_cars', view=views.get_cars, name='getcars'),
+
+    # path for get dealers
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+
+    # path for get dealer by state
+    path(route='get_dealers/<str:state>',view=views.get_dealerships, name='get_dealers_by_state'),
+
+    # path for get dealer details id
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
 
     # path for dealer reviews view
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
 
     # path for add a review view
+    path(route='add_review', view=views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
